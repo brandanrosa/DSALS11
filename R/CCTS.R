8d85b2215eacb07b9472be4734b68980fc92b5fe
Brandan Rosa <brandanrosa22@gmai.com>
2024-10-29 20:03:21 -0500

    Second

---
+++ R/CCTS.R
@@ -0,0 +1,14 @@
+#' CCTS Data
+#'
+#' A complete data set of measurements for CCTS
+#'
+#' @format
+#' A data frame with 144 rows and 5 columns:
+#' \describe{
+#'   \item{CIRCUIT}{Circuit Number}
+#'   \item{STANDARD}{Standard measurment method}
+#'   \item{HUFFMAN}{New method}
+#'   ...
+#' }
+#' @source <https://www.amazon.com/Statistics-Engineering-Sciences-William...
+"CCTS"
